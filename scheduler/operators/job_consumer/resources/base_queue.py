9c1f3a2e1d5a57ba1efd3ec33548446430c26f8c
sirius <oceanus11034@gmail.com>
2020-06-11 16:34:35 +0800

    feat: add queue scheduling: bisect

--- scheduler/operators/job_consumer/resources/base_queue.py
+++ scheduler/operators/job_consumer/resources/base_queue.py
@@ -6,12 +6,37 @@ import abc
 from typing import List, Deque
 from collections import deque
 import heapq
+import bisect
 
 from config import QUEUE_SCHEDULE_CONFIG
 from operators.job_consumer.resources.base_job import Job
 
 
 class BaseStagingList:
+    def __init__(self, level):
+        self.level = level
+
+        # for job storaging
+        self.job_list: List[Job] = []
+
+    @abc.abstractmethod
+    def insert(self, job: Job) -> None:
+        pass
+
+    @abc.abstractmethod
+    def pop(self) -> Job:
+        pass
+
+    @abc.abstractmethod
+    def renew_jobs_priority(self) -> None:
+        self.job_list = list(map(lambda job: job.renew_priority(), self.job_list))
+
+    @abc.abstractmethod
+    def tolist(self) -> List[Job]:
+        return self.job_list
+
+
+class DequeStagingList:
     """ Job Queue for buffering each level job request before assigning to worker
         Basic Version
     """
@@ -26,13 +51,11 @@ class BaseStagingList:
         # for job storaging
         self.job_list: Deque[Job] = deque([])
 
-    @abc.abstractmethod
     def insert(self, job: Job) -> None:
         """ insert the latest job into this list
         """
         self.job_list.append(job)
 
-    @abc.abstractmethod
     def pop(self) -> Job:
         """ get the most urgent job for worker to operate
         """
@@ -43,7 +66,6 @@ class BaseStagingList:
         """
         self.job_list = deque(map(lambda job: job.renew_priority(), self.job_list))
 
-    @abc.abstractmethod
     def tolist(self) -> Deque[Job]:
         """ return a priority sorted list for job selector iterating and pick a valid job
         """
@@ -88,10 +110,34 @@ class HeapStagingList:
         return sorted(self.job_list)
 
 
+class BisectStagingList(BaseStagingList):
+    """ Staging list based on bisect insort
+    """
+
+    def insert(self, job: Job) -> None:
+        """ insert the latest job into this heap
+        """
+        bisect.insort_right(self.job_list, job)
+
+    def pop(self) -> Job:
+        """ get the most urgent job for worker to operate
+        """
+        return self.job_list.pop()
+
+    def remove(self, job: Job):
+        """ remove specific job from list
+        """
+        self.job_list.remove(job)
+
+
 def get_staging_list():
     """ Choose Type of staging list based on .env
         Each staging list get diff sort method or data structure
     """
-    queue_map = {"basic": BaseStagingList, "heap": HeapStagingList}
+    queue_map = {
+        "deque": DequeStagingList,
+        "heap": HeapStagingList,
+        "bisect": BisectStagingList,
+    }
 
     return queue_map[QUEUE_SCHEDULE_CONFIG["STAGE_QUEUE"]]
